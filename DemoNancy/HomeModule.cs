9b179e577210c84f31a2c7f76598d3ad5928c3ca
daijinming <daijinming@live.cn>
2016-12-23 10:03:16 +0800

    这是首次总结
    这是首次描述

---
+++ DemoNancy/HomeModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace DemoNancy
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {   
+
+            this.Get["/"] = _ => {
+                
+                return this.Response.AsText("Hello Nancy");
+            };
+
+            this.Get["/abc"] = _ =>
+            {
+
+                return this.Response.AsText("Hello abc");
+            };
+
+            this.Post["/abc"] = _ =>
+            {
+
+                return this.Response.AsText("Hello abc,POST");
+            };
+        }
+    }
+}
---
+++ DemoNancy/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoNancy.Startup))]
+namespace DemoNancy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.UseNancy();
+        }
+    }
+}
